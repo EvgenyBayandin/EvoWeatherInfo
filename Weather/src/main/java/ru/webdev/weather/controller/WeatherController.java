88a5c9c4a2358e26014cff22d3684c90fe9ca66f
Evgeny Bayandin <5087081@gmail.com>
2024-06-09 11:37:54 +0300

    Modified @GetMapping in WeatherController. Давайте теперь будем принимать в запросе широту и долготу, а возвращать класс Main из пакета model, для упрощения кода. Также запишем путь к серверу погоды в переменную url.weather в файл application.properties. Также добавим в него номер порта сервера, чтобы не конфликтовать с сервисом person.

--- Weather/src/main/java/ru/webdev/weather/controller/WeatherController.java
+++ Weather/src/main/java/ru/webdev/weather/controller/WeatherController.java
@@ -2,8 +2,11 @@ package ru.webdev.weather.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import ru.webdev.weather.model.Main;
 import ru.webdev.weather.model.Root;
 
 @RestController
@@ -15,8 +18,12 @@ public class WeatherController {
     @Value("${appid}")
     String appid;
 
-    @org.springframework.web.bind.annotation.GetMapping("/weather")
-    public Root getWeather(){
-        return restTemplate.getForObject("https://api.openweathermap.org/data/2.5/weather?lat=55.75&lon=37.61&units=metric&appid=" + appid, Root.class);
+    @Value("${url.weather}")
+    String urlWeather;
+
+    @GetMapping("/weather")
+    public Main getWeather(@RequestParam String lat, @RequestParam String lon){
+        String request = String.format("%s?lat=%s&lon=%s&units=metric&appid=%s", urlWeather, lat, lon, appid);
+        return restTemplate.getForObject(request, Root.class).getMain();
     }
 }
