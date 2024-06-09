f9a43d68312c7f55e481c616ad779093b7e82723
Evgeny Bayandin <5087081@gmail.com>
2024-06-09 11:16:52 +0300

    Add package Configuration. Создадим класс AppConfiguration и создадим в нём метод, возвращающий экземпляр RestTemplate, тогда объект создастся один раз при старте приложения. Применим @Autowired для RestTemplate в классе WeatherController.

--- Weather/src/main/java/ru/webdev/weather/controller/WeatherController.java
+++ Weather/src/main/java/ru/webdev/weather/controller/WeatherController.java
@@ -1,5 +1,6 @@
 package ru.webdev.weather.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -8,6 +9,7 @@ import ru.webdev.weather.model.Root;
 @RestController
 public class WeatherController {
 
+    @Autowired
     RestTemplate restTemplate = new RestTemplate();
 
     @Value("appId")
