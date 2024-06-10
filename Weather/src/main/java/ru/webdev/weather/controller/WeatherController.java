7dec997add1f778282ecc1ba18b0299ce56c6c81
Evgeny Bayandin <5087081@gmail.com>
2024-06-10 09:59:18 +0300

    Add CacheManager and @Cacheable(value = "weather-cache", key = "#lat+'_'+#lon") to @GetMapping("/weather")

--- Weather/src/main/java/ru/webdev/weather/controller/WeatherController.java
+++ Weather/src/main/java/ru/webdev/weather/controller/WeatherController.java
@@ -2,6 +2,8 @@ package ru.webdev.weather.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +23,12 @@ public class WeatherController {
     @Value("${url.weather}")
     String urlWeather;
 
+    @Autowired
+    private CacheManager cacheManager;
+
+
     @GetMapping("/weather")
+    @Cacheable(value = "weather-cache", key = "#lat+'_'+#lon")
     public Main getWeather(@RequestParam String lat, @RequestParam String lon){
         String request = String.format("%s?lat=%s&lon=%s&units=metric&appid=%s", urlWeather, lat, lon, appid);
         return restTemplate.getForObject(request, Root.class).getMain();
