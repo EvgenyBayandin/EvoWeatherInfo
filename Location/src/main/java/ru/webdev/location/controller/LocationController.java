cf6b3b80b6a260d5447a1b1dfc7321555cbc2168
Evgeny Bayandin <5087081@gmail.com>
2024-06-10 15:06:50 +0300

    Add LocationController.java, add findByName() to GeodataRepository

---
+++ Location/src/main/java/ru/webdev/location/controller/LocationController.java
@@ -0,0 +1,31 @@
+package ru.webdev.location.controller;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import ru.webdev.location.model.Geodata;
+import ru.webdev.location.repository.GeodataRepository;
+
+@RestController
+@RequestMapping("/location")
+public class LocationController {
+
+    @Autowired
+    private GeodataRepository repository;
+
+    @GetMapping
+    public Optional<Geodata> getLocation(@RequestParam String name){
+        return repository.findByName(name);
+    }
+
+    @PostMapping
+    public Geodata save(@RequestBody Geodata geodata){
+        return repository.save(geodata);
+    }
+
+}
