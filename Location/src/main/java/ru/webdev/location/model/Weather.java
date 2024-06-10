e7a5e338800ce5dfda4d5b3ae61e42f0e01f26c1
Evgeny Bayandin <5087081@gmail.com>
2024-06-10 14:51:50 +0300

    Add new module Location, add package Model, create Weather.java

---
+++ Location/src/main/java/ru/webdev/location/model/Weather.java
@@ -0,0 +1,15 @@
+package ru.webdev.location.model;
+
+@lombok.NoArgsConstructor
+@lombok.AllArgsConstructor
+@lombok.Data
+public class Weather {
+    private double temp;
+    private double feels_like;
+    private double temp_min;
+    private double temp_max;
+    private int pressure;
+    private int humidity;
+    private int sea_level;
+    private int grnd_level;
+}
