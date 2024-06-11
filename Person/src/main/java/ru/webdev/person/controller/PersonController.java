071e572bb08cf96ad7c903a02b965f2277c2c44c
Evgeny Bayandin <5087081@gmail.com>
2024-06-11 17:23:48 +0300

    5.8 Задание 22. Обнаружение служб. Задание 3. Добавить методы в API как указано ниже: Требования к API: GET /person - Получить всех пользователей List<Person> GET /person/{id} - Получить пользователя Person по id POST /person - Добавить нового пользователя Person PUT /person/{id} - Изменить пользователя Person по id DELETE /person/{id} - Удалить пользователя Person по id GET /person/{id}/weather - Получить погоду для местонахождения пользователя Person по id

--- Person/src/main/java/ru/webdev/person/controller/PersonController.java
+++ Person/src/main/java/ru/webdev/person/controller/PersonController.java
@@ -4,9 +4,11 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +42,25 @@ public class PersonController {
         return repository.findById(person.getId()).isPresent()
                 ? new ResponseEntity(repository.findById(person.getId()), HttpStatus.BAD_REQUEST)
                 : new ResponseEntity(repository.save(person), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public Person putPerson(@PathVariable int id, @RequestBody Person person)  {
+        Person p = repository.findById(id).get();
+        p.setFirstname(person.getFirstname());
+        p.setSurname(person.getSurname());
+        p.setLastname(person.getLastname());
+        p.setLocation(person.getLocation());
+        p.setBirthday(person.getBirthday());
+        Person savedPerson = repository.save(p);
+        return repository.save(savedPerson);
+    }
+
+    @DeleteMapping("/{id}")
+    public Person deletePerson(@PathVariable int id)  {
+        Person p = repository.findById(id).get();
+        repository.delete(p);
+        return p;
     }
 
     @GetMapping("{id}/weather")
