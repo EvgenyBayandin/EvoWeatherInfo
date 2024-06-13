1a9cc6aa573d579df03a6b598602de04d62f3a67
Evgeny Bayandin <5087081@gmail.com>
2024-06-13 13:26:06 +0300

    Removing instances Location and Person, restore application.properties

--- PersonSecond/src/main/java/ru/webdev/person/repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.webdev.person.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import ru.webdev.person.model.Person;
-
-@Repository
-public interface PersonRepository extends CrudRepository<Person, Integer> {
-}
